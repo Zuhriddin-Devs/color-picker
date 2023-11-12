95a527085de6608460d29711bd6b6cc6e31b2123
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-11-12 18:28:06 +0500

    Function for color picker, color code, and copy code is given

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const elColorPicker = document.querySelector('#js-color-picker');
+    const elColorCode = document.querySelector('#js-color-code');
+    const elCopyButton = document.querySelector('#js-copy-btn');
+    const elBody = document.querySelector('body');
+
+    elColorPicker.addEventListener('input', function () {
+        const selectedValue = elColorPicker.value;
+        elBody.style.backgroundColor = selectedValue;
+        elColorCode.innerText = selectedValue;
+    });
+
+    elCopyButton.addEventListener('click', function () {
+        const range = document.createRange();
+        range.selectNode(elColorCode);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand('copy');
+        window.getSelection().removeAllRanges();
+    });
+});
